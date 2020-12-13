42837f1c4130c0ce5c3646649c6f71f8146a485b
Lxandqy <65651714+Lxandqy@users.noreply.github.com>
2020-12-13 07:01:49 +0000

    Update P3379 【模板】最近公共祖先（LCA）倍增法.cpp

--- P3379 【模板】最近公共祖先（LCA）倍增法.cpp
+++ P3379 【模板】最近公共祖先（LCA）倍增法.cpp
@@ -1,53 +1,89 @@
-//模板ovo
-
-
 #include<iostream>
-#include<vector>
+#include<cstring>
+
 using namespace std;
-vector<int>v[500005];
-int depth[500005],fa[500005][22],lg[500005];
-int n,m,s;
-void llgg(){
-	for(int i = 1; i <= n; i++){
-		lg[i] = lg[i - 1] + (1 << lg[i - 1] == i);
+
+const int N = 4e4 + 10;
+int n,m,root;
+
+int head[N],to[N * 2],last[N * 2],cnt;
+void add(int a,int b){
+	to[++cnt] = b;
+	last[cnt] = head[a];
+	head[a] = cnt;
+}
+
+int fa[N][22],depth[N],q[N];
+void bfs(int root){
+	memset(depth,0x3f,sizeof depth);
+	int tt = 0,hh = 0;
+	depth[0] = 0;
+	depth[root] = 1;
+	q[0] = root;
+	while(hh <= tt){
+		int p = q[hh++];
+		for(int i = head[p]; i != -1; i = last[i]){
+			int j = to[i];
+			if(depth[j] > depth[p] + 1){
+				depth[j] = depth[p] + 1;
+				q[++tt] = j;
+				fa[j][0] = p;
+				for(int k = 1; k <= 16; k++){
+					fa[j][k] = fa[fa[j][k - 1]][k - 1];
+				}
+			} 
+		}
 	}
 }
-void dfs(int now,int last){
-	fa[now][0] = last,depth[now] = depth[last] + 1;
-	for(int i = 1; i <= lg[depth[now]]; i++){
-		fa[now][i] = fa[fa[now][i - 1]][i - 1];
-	}
-	for(int i = 0; i < v[now].size(); i++){
-		if(v[now][i] != last) dfs(v[now][i],now);
-	}
-}
+
 int lca(int x,int y){
 	if(depth[x] < depth[y]) swap(x,y);
-	while(depth[x] > depth[y]){
-		x = fa[x][lg[depth[x] - depth[y]] - 1];
-	}//de[x] = 9 ,de[y] = 1, xiangshang 2^2
+	for(int i = 16; i >= 0; i --){
+		if(depth[fa[x][i]] >= depth[y]){
+			x = fa[x][i];	
+		}
+	}
+	
 	if(x == y) return x;
-	for(int k = lg[depth[x]] - 1; k >= 0; --k){
+	for(int k = 16; k >= 0; k--){
 		if(fa[x][k] != fa[y][k]){
 			x = fa[x][k],y = fa[y][k];
 		}
-	} 
-	return fa[x][0]; 
-}
-int main(){
-	ios::sync_with_stdio(false);
-    int x,y;
-	cin >> n >> m >> s;
-	for(int i = 1; i <= n - 1; i++){
-		cin >> x >> y;
-		v[x].push_back(y);
-		v[y].push_back(x);
 	}
-	llgg();
-	dfs(s,0);
+	
+	return fa[x][0];
+}
+
+int main(){
+	memset(head,-1,sizeof head);
+	scanf("%d",&n);
+	for(int i = 1,x,y;i <= n; i++){
+		scanf("%d%d",&x,&y);
+		if(y == -1) root = x;
+		else{
+			add(x,y);
+			add(y,x);
+		}
+	}
+	
+	bfs(root);
+	
+	scanf("%d",&m);
 	for(int i = 1; i <= m; i++){
-		cin >> x >> y;
-		cout << lca(x,y) << endl;
-	} 
+		int x,y;
+		scanf("%d%d",&x,&y);
+		int s = lca(x,y);
+		if(s == x){
+			printf("%d\n",1);
+		}else if(s == y){
+			printf("%d\n",2);
+		}else{
+			printf("%d\n",0);
+		}
+	}
+	
+	
+	
+	
 	return 0;
 }
